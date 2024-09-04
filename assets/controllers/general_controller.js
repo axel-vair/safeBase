71ed38b85ab4a57c745bbecbc9111f74eab79398
Axel <axel@Mac.local>
2024-09-04 14:16:56 +0200

    :sparkles: display database and infos of the database

--- assets/controllers/general_controller.js
+++ assets/controllers/general_controller.js
@@ -6,84 +6,20 @@ export default class extends Controller {
         document.body.classList.toggle('sidebar-collapse')
     }
 
-   /* openInjections() {
-        const injections = document.getElementById('injections')
-        injections.classList.toggle('menu-open')
-    }
+    collapseCard() {
+        const cardBody = this.element.querySelector('.card-body'); // Sélectionne le corps de la carte
+        const isCollapsed = this.element.classList.toggle('collapsed-card'); // Change la classe de la carte
+        const cardFooter = this.element.querySelector('.card-footer');
 
-    openAdministration(){
-        const administration = document.getElementById('administration')
-        administration.classList.toggle('menu-open')
+        // Gère l'affichage du corps de la carte
+        if (isCollapsed) {
+            cardBody.style.display = 'none'; // Masque le corps de la carte
+            cardFooter.style.display = 'none';
 
-    }
-
-    tab(event){
-        const navLinks = this.element.querySelectorAll('.nav-link');
-        const tabId = event.target.dataset.id;
-        const informationsTableau = document.getElementById('informations-tableau');
-        const abonnementsTableau = document.getElementById('abonnements-tableau');
-        const verificationsTableau = document.getElementById('verifications-tableau');
-
-
-        navLinks.forEach(link => {
-            if (link.dataset.id === tabId) {
-                link.classList.add('active');
-            } else {
-                link.classList.remove('active');
-            }
-        });
-
-        if (tabId === 'informations') {
-            informationsTableau.style.display = 'block';
         } else {
-            informationsTableau.style.display = 'none';
-        }
+            cardBody.style.display = 'block'; // Affiche le corps de la carte
+            cardFooter.style.display = 'block';
 
-        if (tabId === 'abonnements') {
-            abonnementsTableau.style.display = 'block';
-        } else {
-            abonnementsTableau.style.display = 'none';
-        }
-
-        if (tabId === 'verifications') {
-            verificationsTableau.style.display = 'block';
-        } else {
-            verificationsTableau.style.display = 'none';
         }
     }
-
-    tabVeille(event){
-        const navLinks = this.element.querySelectorAll('.nav-link');
-        const tabId = event.target.dataset.id;
-        const informationsVeille = document.getElementById('informations-veille');
-        const abonnesVeille = document.getElementById('abonnes-veille');
-        const statistiquesVeille = document.getElementById('statistiques-veille');
-
-        navLinks.forEach(link => {
-            if (link.dataset.id === tabId) {
-                link.classList.add('active');
-            } else {
-                link.classList.remove('active');
-            }
-        });
-
-        if (tabId === 'informations-veille') {
-            informationsVeille.style.display = 'block';
-        } else {
-            informationsVeille.style.display = 'none';
-        }
-
-        if (tabId === 'abonnes-veille') {
-            abonnesVeille.style.display = 'block';
-        } else {
-            abonnesVeille.style.display = 'none';
-        }
-
-        if (tabId === 'statistiques-veille') {
-            statistiquesVeille.style.display = 'block';
-        } else {
-            statistiquesVeille.style.display = 'none';
-        }
-    }
-*/
 }
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,36 @@ use Symfony\Component\Routing\Attribute\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $connection = $entityManager->getConnection();
+        $params = $connection->getParams();
+        $databaseName = $params['dbname'] ?? 'Nom de la base de données non défini';
+
+        $schemaManager = $connection->getSchemaManager();
+        $tables = $schemaManager->listTables();
+
+        $columns = [];
+        foreach ($tables as $table) {
+            $tableName = $table->getName();
+            if (!in_array($tableName, ['doctrine_migration_versions', 'messenger_messages'])) {
+                $tableColumns = $schemaManager->listTableColumns($tableName);
+                $columns[$tableName] = array_map(function ($column) {
+                    return $column->getName();
+                }, $tableColumns);
+            }
+        }
+
+
+        // Vérifier si la connexion est active
+        $isConnected = $connection->isConnected();
+
+
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'database' => $databaseName,
+            'tables' => $tables,
+            'isConnected' => $isConnected,
+            'columns' => $columns,
         ]);
     }
 }
